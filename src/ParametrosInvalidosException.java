f0b8a28ab651f4efb65aa3bfd412275a57dc1cbd
Lucas de Amorim <112043236+Luckyx128@users.noreply.github.com>
2024-09-02 14:19:13 -0300

    Contador com fluxo de interações

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+    /**
+    *Exception personalizada para casos do valor do primeiro numero ser menor que o segundo numero
+     */
+public class ParametrosInvalidosException extends Throwable {
+}
